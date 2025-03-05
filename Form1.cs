881f307c5bc8dd2f8958c3f621906523cd61f109
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-05 07:43:08 +0700

    Menambahkan Layout GUI Kalkulator

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,60 @@ namespace Modul3_103022300136
         {
             InitializeComponent();
         }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
